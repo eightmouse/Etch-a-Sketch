2c4c67b2666a9a243dffbd2c2750d3ac67326aa0
eightmouse <92474424+eightmouse@users.noreply.github.com>
2023-06-26 16:31:20 +0200

    Added 2 onclick event to the buttons

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 document.addEventListener("DOMContentLoaded",function(){
-    //Changes the amount of columns & rows
-    createBoard(32);
+    //Calling the functions
+    createBoard(16); //This changes the size of the board
+    //This prompts a msg to ask for the size
+    let btn_popup = document.querySelector("#popup");
+    btn_popup.addEventListener("click", function(){
+        let size = getSize();
+        createBoard(size);
+    })
     //Just a console log, dont mind me
     console.log("Henlo human, plx no mess with DOM plx")
 }) 
@@ -17,11 +23,20 @@ function createBoard(size){
 
     for (let i = 0; i < numDivs; i++){
         let div = document.createElement("div");
-
-        /*
-        div.style.backgroundColor = "yellow";
-        */
-
+        div.style.backgroundColor = "white";
         board.insertAdjacentElement("beforeend", div)
     }
+}
+
+function getSize(){
+    let input = prompt("What will be the size of the canvass?")
+    let message = document.querySelector("#message");
+    if(input == ""){
+        message.innerHTML = "Please provide a number";
+    } else if(input < 0 || input > 100){
+        message.innerHTML = "Please provide a number between 1 and 100 :("
+    } else {
+        message.innerHTML = "Have fun!"
+    }
+
 }
